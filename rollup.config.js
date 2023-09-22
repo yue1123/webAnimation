2eb8820936f8bc48ead6f313cf807ad3b4ff625c
yue1123 <2572468699@qq.com>
2023-09-23 01:24:25 +0800

    chore: build config improve

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,73 @@ const { defineConfig } = require('rollup')
 const typescript = require('@rollup/plugin-typescript')
 const resolve = require('rollup-plugin-node-resolve')
 const dts = require('rollup-plugin-dts').default
+const { terser } = require('rollup-plugin-terser')
+const replace = require('@rollup/plugin-replace')
+const pkg = require('./package.json')
+const concat = require('rollup-plugin-concat')
+
+const sharedPlugins = [
+  replace({
+    values: {
+      __DEV__: "process.env.NODE_ENV !== 'production'",
+      __VERSION__: pkg.version
+    },
+    preventAssignment: true
+  }),
+  typescript(),
+  resolve()
+]
+const baseConfig = { exports: 'named' }
+const baseEsmConfig = { ...baseConfig, format: 'esm' }
+const baseUmdConfig = { ...baseConfig, format: 'umd', name: 'WebAnimation' /** export name */ }
 
 module.exports = defineConfig([
+  // {
+  //   input: './src/index.ts',
+  //   output: [
+  //     {
+  //       ...baseEsmConfig,
+  //       file: './dist/index.esm.js'
+  //     },
+  //     { ...baseEsmConfig, file: './dist/index.esm.min.js', plugins: [terser()] },
+  //     { ...baseUmdConfig, file: './dist/index.umd.js' },
+  //     { ...baseUmdConfig, file: './dist/index.umd.js', plugins: [terser()] }
+  //   ],
+  //   plugins: sharedPlugins
+  // },
   {
     input: './src/index.ts',
+    external: /animations|easings/,
     output: {
-      exports: 'named',
-      format: 'esm',
+      ...baseEsmConfig,
       dir: 'dist'
+    },
+    plugins: sharedPlugins
+  },
+  {
+    input: './src/animations/index.ts',
+    output: {
+      ...baseEsmConfig,
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      dir: 'dist'
+    },
+    plugins: sharedPlugins
+  },
+  {
+    input: './src/easings/index.ts',
+    output: {
+      ...baseEsmConfig,
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      dir: 'dist'
+    },
+    plugins: sharedPlugins
+  },
+  {
+    input: './src/polyfill.ts',
+    output: {
+      file: './dist/polyfill.min.js'
     },
     plugins: [typescript(), resolve()]
   },
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from './animate'
 export * from './animations'
 export * from './easings'
+
+export const version = '__VERSION__'
